aa5acdcc2e56292bbf48fd9b1523998a651f0608
mjq4my <michael.quinn808@gmail.com>
2024-05-14 21:07:40 -0400

    updates

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import resume from '../../assets/resume.pdf'
+import file from '../../assets/file.pdf'
 
 // ########### CTA = Call to Action ###########
 
@@ -7,7 +7,7 @@ const CTA = () => {
   return (
     <div className='cta'>
         <a href = "#contact" className='btn'>Let's Talk</a>
-        {/* <a href = {resume} download className='btn btn-primary'>Download Resume</a> */}
+        <a href = {file} download className='btn btn-primary'>Download Resume</a>
     </div>
   )
 }
